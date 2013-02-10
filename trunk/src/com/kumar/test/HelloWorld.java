b4d19f34bcd9f35a2d02b67f49a80c954ddc1572
kumar.keswani@gmail.com <kumar.keswani@gmail.com@d61dd8b1-7065-c7ad-79c0-88609e4ad0c8>
2013-02-10 04:14:58 +0000

    Added new test package

---
+++ trunk/src/com/kumar/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.kumar.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kumar
+ * Date: 2/9/13
+ * Time: 11:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld Test!");
+    }
+}
