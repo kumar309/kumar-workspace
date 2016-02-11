09efe9fe7b6fbb779a161ff6869c679a895a370b
kumar309 <kumar.keswani@gmail.com>
2016-02-11 00:02:06 -0500

    Adding a new Print Statement

--- src/com/kumar/test/HelloWorld.java
+++ src/com/kumar/test/HelloWorld.java
@@ -11,5 +11,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("HelloWorld Test!");
+        System.out.println("created a branch called bugfix/inheritence");
     }
 }
